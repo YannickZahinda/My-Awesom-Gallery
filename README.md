976167b546f505cfcbabe226fdcc2516fe2b0ed2
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-05-01 00:34:08 +0200

    get image from api

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # My-Awesom-Gallery
 
 My awesome gallerie is a simple app that gives you a great experience of high qualities pics
+
+# Build with
+
+HTML, CSS, JAVASCRIPT, WEBPACK
+
+# Getting started
+```
+git clone 
+cd My-Awesome-Gallery
+npm install : to install all dependecies
+```
+
+# Run in your browser
+```
+npm start
+```
+
+# Author
+
+# Give a start to show some support
--- src/modules/galery.js
+++ src/modules/galery.js
@@ -1,15 +1,18 @@
 const getDataFromApi = async() => {
     const url = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Spaghetti');
     const response = await url.json();
-    const mealContainer = document.querySelectorAll('.img-container');
+    const mealContainer = document.querySelectorAll('img');
     // console.log(response.meals[0].strMealThumb);
     let mealArray = [];
     mealArray.push(response.meals);
     // console.log(mealArray[0]);
     mealArray.filter((meal) => {
         mealContainer.forEach(element => {
-            const mealImgSrc = meal.filter((elt) => elt.strMealThumb);
-            element.innerHTML = mealImgSrc.src;
+            meal.filter((elt) =>{
+                let imgArray = [];
+                imgArray.push(elt.strMealThumb)
+               element.src = imgArray;
+            });
         });
     })
 
